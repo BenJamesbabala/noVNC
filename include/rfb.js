36f35a764500619f32c1ccc87e052a79b5d77b9d
Samuel Mannehed <samuel@cendio.se>
2016-09-19 00:32:48 +0200

    Rename cleanupSocket() to cleanup()
    It doesn't only cleanup the websocket.

--- include/rfb.js
+++ include/rfb.js
@@ -237,7 +237,7 @@ var RFB;
         });
 
         this._init_vars();
-        this._cleanupSocket();
+        this._cleanup();
 
         var rmode = this._display.get_render_mode();
         if (Websock_native) {
@@ -402,7 +402,7 @@ var RFB;
             }
         },
 
-        _cleanupSocket: function () {
+        _cleanup: function () {
             if (this._msgTimer) {
                 clearInterval(this._msgTimer);
                 this._msgTimer = null;
@@ -452,7 +452,7 @@ var RFB;
             this._onUpdateState(this, state, oldstate);
             switch (state) {
                 case 'disconnected':
-                    this._cleanupSocket(); // Make sure we are closed.
+                    this._cleanup(); // Make sure we are closed.
 
                     if (typeof(errorMsg) !== 'undefined') {
                         this._onDisconnected(this, errorMsg);
@@ -470,7 +470,7 @@ var RFB;
                 case 'connecting':
                     // A previous connect may asynchronously cause a connection
                     // so make sure we are still closed.
-                    this._cleanupSocket();
+                    this._cleanup();
                     this._init_vars();
 
                     // WebSocket.onopen transitions to the RFB init states
@@ -479,7 +479,7 @@ var RFB;
 
                 case 'disconnecting':
                     // WebSocket.onclose transitions to 'disconnected'
-                    this._cleanupSocket();
+                    this._cleanup();
 
                     this._disconnTimer = setTimeout(function () {
                         this._fail("Disconnect timeout");
